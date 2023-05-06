0c9ebcc23756734146ba327d3b5a219710a6fed8
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 11:58:57 -0500

    Java Syntax

---
+++ JavaTutorial/javaSyntax.java
@@ -0,0 +1,5 @@
+class output{
+	public static void main(String []args){
+		System.out.println("Debemos de tener cuidado en la sintaxis del nombre de la clase y el nombre del archivo, ya que java es case-sensitive");
+	}
+}

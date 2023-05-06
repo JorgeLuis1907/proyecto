8cdb44bcc0a4647a11e519bce7b2ecd458c68dda
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 12:28:34 -0500

    Java Variables

---
+++ JavaTutorial/javaVariables.java
@@ -0,0 +1,14 @@
+class variables{
+	public static void main(String [] args){
+		String nombre="Jorge";
+		int numero=10;
+		int numero2;
+		numero2=100;
+		final int numero3=15;
+		boolean booleano=true;
+		float flotante=15.7f;
+		char espacio=32;
+		System.out.println("Aqui hay un ejemplo de cada tipo de variable mostrado en la seccion: \n" + numero+"\n"+numero2+"\n"+numero3+"\n"+booleano+"\n"+flotante+"\n"+espacio);
+		System.out.println("Puedes declarar varias variables de esta manera int a=0,b=1,c=2 o tambien int a,b,c , pero recordando siempre que el nombre de las variables deberian de estar referenciando a los valores que tomaran, por ejemplo int edad, telefono, dni.");
+	}
+}

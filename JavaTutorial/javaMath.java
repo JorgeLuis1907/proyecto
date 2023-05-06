20f39be004374c083a4e644ec7e992cf24a533f3
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 13:37:54 -0500

    Java Math

---
+++ JavaTutorial/javaMath.java
@@ -0,0 +1,10 @@
+class math{
+	public static void main(String [] args){
+		int maximo=Math.max(10,5);		//el maximo de 10 y 5
+		int minimo=Math.min(10,5);		//el minimo de 10 y 5
+		double raiz=Math.sqrt(25);		//la raiz cuadrada de 25
+		double x=Math.abs(-10.5);		//devuelve el valor absoluto del argumento que seria 10.5
+		int random=(int)(Math.random()*10);	//un numero random entre 0 y 9 casteado a entero
+
+	}
+}

fda82a022e6a9a2eabd782b83bacc6d9ec5ffc7a
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 11:55:39 -0500

    Java Get Started, en el que estaba la instalacion de java

---
+++ JavaTutorial/javaInstall.java
@@ -0,0 +1,6 @@
+class install{
+	public static void main(String [] args){
+		System.out.println("Para instalar java, debes de primero saber que tipo de ordenador tienes, y luego aplicar la instalacion adecuada.");
+		System.out.println("Hello World");
+	}
+}

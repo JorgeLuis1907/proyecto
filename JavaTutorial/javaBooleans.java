4b3e22ed8ff3d97a66126a0039f6e23455e38491
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 13:45:19 -0500

    Java Booleans

---
+++ JavaTutorial/javaBooleans.java
@@ -0,0 +1,11 @@
+class booleans{
+	public static void main(String[]args){
+		int edad1=19;
+		int edad2=16;
+		int edad3=20;
+		int edad4=15;
+		System.out.println("¿Edad1 = 19 es mayor de edad? "+(edad1>=18));
+		System.out.println("¿Edad2 = 16 es mayor que Edad3 = 20? "+(edad2>edad3));
+		System.out.println("¿Edad4 = 15 tiene 15 años? "+(edad4==15));
+	}
+}

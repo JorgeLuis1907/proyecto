20bec47d28810aa1f21d5760e31e9ad87b371dd0
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 12:28:11 -0500

    Java Output

---
+++ JavaTutorial/javaOutput.java
@@ -0,0 +1,7 @@
+class output{
+	public static void main(String []args){
+		System.out.println("Primero se tiene la impresion de texto, con el metodo println(), el cual va a darnos un salto de linea al final del string \n Luego tenemos el metodo print() el cual no nos dara ese salto extra"); 
+		System.out.println("Segundo tenemos la impresion de numeros en el cual nos aclara que las operaciones dentro de los parentesis del metodo print se desarrollan antes de su impresion como println(2*5) escribiria 10");
+	
+	}
+}

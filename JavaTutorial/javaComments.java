9d9790b9d7d98f753d8ff28a221a028fe693f14d
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 12:27:41 -0500

    Java Comments

---
+++ JavaTutorial/javaComments.java
@@ -0,0 +1,9 @@
+class comments{
+	public static void main(String []args){
+		//este es un comentario en java
+		/*Este es un comentario 
+		 * mas extenso 
+		 * en java*/
+		System.out.println("con // se puede colocar comentarios, con /* */ todo lo escrito dentro sera un comentario");
+	}
+}

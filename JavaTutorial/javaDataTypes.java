dbf5337cc7439c496bf0ce51240c480f42228858
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 12:45:00 -0500

    Java Data Types

---
+++ JavaTutorial/javaDataTypes.java
@@ -0,0 +1,15 @@
+class dataType{
+	public static void main(String []args){
+		char caracter=32;	//Character valor ASCII del caracter que se quiere
+		boolean booleano=true;	//Boolean pudiendo ser true o fals
+		String texto="Hello";	//String texto
+		byte numChico=10;	//byte  -128 hasta 127
+		short numMediano=2000;	//short -32768 hasta 32767
+		int numGrande=900000;	//int -2147483648 hasta 2147483647
+		long numGigante;	//-9223372036854775808 to 9223372036854775807
+		float flotante=15f;	//float precision de 6-7 digitos despues del punto
+		double valor=15d;	//double precision hasta 15 digitos despues del punto
+	System.out.println("Aqui se encuentran los tipos de datos que hay en java, asi como una pequeña descripcion de cada uno");
+		
+	}
+}

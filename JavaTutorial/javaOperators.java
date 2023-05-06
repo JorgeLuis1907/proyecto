8ce0278be85ed8160b9b4846b6f11c9b19c9fd39
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-06 13:01:55 -0500

    Java Operators

---
+++ JavaTutorial/javaOperators.java
@@ -0,0 +1,13 @@
+class operators{
+	public static void main(String []args){
+		int n1=100;
+		int n2=200;
+		int incremento=n1++;
+		int decremento=n1--;
+		n1+=n2;//realiza la suma n1+n2 y se la asigna a n1
+		n1-=n2;//realiza la resta n1-n2 y se la asigna a n1
+		n1*=n2;//realiza la multiplicacion n1*n2 y la asigna a n1
+		n1/=n2;//realiza la division n1/n2 y la asigna a n1
+		
+	}
+}	

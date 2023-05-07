0abd9b01830470f7d3a9309cb9ffd19f658f77f9
Jorge Pachari <jpachari@unsa.edu.pe>
2023-05-07 07:31:07 -0500

    Primer intento de metodos, sin terminar

---
+++ javaMethodsOverloading.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class methodsOverloading{
+	public static void main(String[]args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Ingrese double ");
+		double a=sc.nextDouble();
+		System.out.println("Ingrese double");
+		double b=sc.nextDouble();
+		double r1=retorno(a,b);
+		System.out.println("Ingrese dos numeros para multiplicar");
+		int numero1=sc.nextInt();
+		int numero2=sc.nextInt();
+		int r2=retorno(numero1, numero2);
+		System.out.pritnStream("La division de los numeros double es:  " + r1);
+		System.out.printStream("El producto de los numeros enteros es: " + r2);
+	}
+	static double retorno(double a, double b){
+		return a/b;
+	}
+	static int retorno(int a, int b){
+		return a*b;
+	}
+}
